c78be2aa3ba0b50699974afff3853aa1a2177c07
Ho Xuan Vu Huy <huyhxv@gmail.com>
2017-06-07 20:43:42 +0700

    Hoan thanh dang nhap

--- DoAn_Auction/Controllers/AccountController.cs
+++ DoAn_Auction/Controllers/AccountController.cs
@@ -84,5 +84,114 @@ namespace DoAn_Auction.Controllers
                 return check;
             }
         }
+        // POST: Account/Login
+        [HttpPost]
+        public ActionResult Login(LoginVM model)
+        {
+            using (var ctx = new QLDauGiaEntities())
+            {
+                if(model.RawPWD==null || model.Username==null)
+                {
+                    TempData["ErrorMsg"] = "Đăng nhập không thành công";
+                    return RedirectToAction("Register", "Account");
+                }
+                string encPwd = StringUtils.MD5(model.RawPWD);
+                var user = ctx.Users
+                    .Where(u => u.f_Username == model.Username && u.f_Password == encPwd)
+                    .FirstOrDefault();
+                if (user != null)
+                {
+                    Session["isLogin"] = 1;
+                    Session["user"] = user;
+
+                    if(model.Remember)
+                    {
+                        Response.Cookies["userID"].Value = user.f_ID.ToString();
+                        Response.Cookies["userID"].Expires = DateTime.Now.AddDays(7);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["ErrorMsg"] = "Đăng nhập không thành công";
+                    return RedirectToAction("Register", "Account");
+                }
+            }
+        }
+
+        // POST: Account/Logout
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            CurrentContext.Destroy();
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Account/Profile
+        [CheckLogin]
+        public ActionResult Profile()
+        {
+            var ctx = new QLDauGiaEntities();
+            int ID =(int) CurrentContext.GetCurUser().f_ID;
+            var user = ctx.Users.Where(p => p.f_ID == ID).FirstOrDefault();
+            return View(user);
+        }
+
+        // GET: Account/EditProfile
+        [CheckLogin]
+        public ActionResult EditProfile()
+        {
+            return View();
+        }
+
+        // POST: Account/EditProfile
+        [CheckLogin]
+        [HttpPost]
+        public ActionResult EditProfile(RegisterVM model)
+        {
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var user = ctx.Users.Where(p => p.f_Username == model.Username).FirstOrDefault();
+                if(user.f_Password==StringUtils.MD5(model.RawPWD))
+                {
+                    user.f_Name = model.Name;
+                    user.f_Email = model.Email;
+                    user.f_DOB = DateTime.ParseExact(model.DOB, "d/m/yyyy", null);
+                    user.f_Address = model.Address;
+                    user.f_Phone = model.Phone;
+                }
+
+                ctx.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+            }
+            return RedirectToAction("Profile","Account");
+        }
+
+        // GET: Account/EditProfile
+        [CheckLogin]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        // POST: Account/EditProfile
+        [HttpPost]
+        public ActionResult ChangePassword(string RawPWD,string NewRawPWD)
+        {
+            int ID = (int)CurrentContext.GetCurUser().f_ID;
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var user = ctx.Users.Where(p => p.f_ID ==ID).FirstOrDefault();
+                if (user.f_Password == StringUtils.MD5(RawPWD))
+                {
+                    user.f_Password = StringUtils.MD5(NewRawPWD);
+                    ctx.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                    ctx.SaveChanges();
+                    CurrentContext.Destroy();
+                    return RedirectToAction("Index", "Home");
+                }
+                return View();
+            }
+        }
     }
 }
